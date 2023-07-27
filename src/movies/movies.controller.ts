047f19dcdf6e76f085566305b66fabd8c69f264c
재명 <ljm9894@dgsw.hs.kr>
2023-07-28 00:39:09 +0900

    service entities controller study

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, Param, Post, Delete , Patch, Body, Query} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+    constructor(private readonly movieService: MoviesService){
+
+    }
+    @Get()
+    getAll() : Movie[]{
+        return this.movieService.getAll();
+    }
+    @Get("search")
+    search(@Query("year") searchingYear: string){
+        return `We are searching for a movie made `
+    }
+
+    @Get("/:id")
+    getOne(@Param('id') movieId: string) : Movie{
+        return this.movieService.getOne(movieId);
+    }
+
+    @Post()
+    create(@Body() movieData) {
+        return this.movieService.create(movieData);
+    }
+
+    @Delete("/:id")
+    remove(@Param('id') movieId : string){
+        return this.movieService.deleteOne(movieId);
+    }
+    @Patch('/:id')
+    patch(@Param('id') movieId : string, @Body() updateData){
+        return {
+            updatedMovie : movieId,
+            ...updateData
+        }
+    }
+    
+    
+}
